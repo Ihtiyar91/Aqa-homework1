547fc6db6f896d1c376d3ba931adc12a72e298ed
Ihtiyar91 <ikhtiyargaliev@gmail.com>
2022-07-29 18:24:28 +0300

    Creat test and  found bug

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,65 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testBonus() {
+        CashbackHackService cashback= new CashbackHackService();
+        int amount=800;
+        int actual= cashback.bonus (amount);
+        int expected=200;
+        assertEquals ( actual,expected);
+    }
+    @Test
+    public void testMinBuy() {
+        CashbackHackService cashback= new CashbackHackService();
+        int amount=1;
+        int actual= cashback.bonus (amount);
+        int expected=999;
+        assertEquals ( actual,expected);
+    }
+    @Test
+    public void testNotBuy() {
+        CashbackHackService cashback= new CashbackHackService();
+        int amount=0;
+        int actual= cashback.bonus (amount);
+        int expected=1000;
+        assertEquals ( actual,expected);
+    }
+    @Test
+    public void testNegativBonus() {
+        CashbackHackService cashback= new CashbackHackService();
+        int amount=-1;
+        int actual= cashback.bonus (amount);
+        int expected=1001;
+        assertEquals ( actual,expected);
+    }
+    @Test
+    public void testMaxBuyForBonus() {
+        CashbackHackService cashback= new CashbackHackService();
+        int amount=999;
+        int actual= cashback.bonus (amount);
+        int expected=1;
+        assertEquals ( actual,expected);
+    }
+    @Test
+    public void testNotMessangeBonus() {
+        CashbackHackService cashback= new CashbackHackService();
+        int amount=1000;
+        int actual= cashback.bonus (amount);
+        int expected=0;
+        assertEquals ( actual,expected);
+    }
+    @Test
+    public void testNotMessangeBonus2() {
+        CashbackHackService cashback= new CashbackHackService();
+        int amount=1001;
+        int actual= cashback.bonus (amount);
+        int expected=0;
+        assertEquals ( actual,expected);
+    }
+}
