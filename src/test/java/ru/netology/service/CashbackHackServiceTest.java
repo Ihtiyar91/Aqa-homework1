3b7a6c0f8dff384c525947be7a55e00a4a089040
Ihtiyar91 <ikhtiyargaliev@gmail.com>
2022-08-01 13:10:08 +0300

    Creat JUnit5 and Legacy

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void testBonus() {
+    public void shouldBonus200() {
         CashbackHackService cashback= new CashbackHackService();
         int amount=800;
         int actual= cashback.bonus (amount);
@@ -15,7 +15,7 @@ public class CashbackHackServiceTest {
         assertEquals ( expected,actual);
     }
     @Test
-    public void testMinBuy() {
+    public void shouldBonus999() {
         CashbackHackService cashback= new CashbackHackService();
         int amount=1;
         int actual= cashback.bonus (amount);
@@ -23,7 +23,7 @@ public class CashbackHackServiceTest {
         assertEquals ( expected,actual);
     }
     @Test
-    public void testNotBuy() {
+    public void shouldBonus1000() {
         CashbackHackService cashback= new CashbackHackService();
         int amount=0;
         int actual= cashback.bonus (amount);
@@ -31,7 +31,7 @@ public class CashbackHackServiceTest {
         assertEquals ( expected,actual);
     }
     @Test
-    public void testNegativBonus() {
+    public void shouldNegativBonus() {
         CashbackHackService cashback= new CashbackHackService();
         int amount=-1;
         int actual= cashback.bonus (amount);
@@ -39,7 +39,7 @@ public class CashbackHackServiceTest {
         assertEquals ( expected,actual);
     }
     @Test
-    public void testMaxBuyForBonus() {
+    public void shouldBonus1() {
         CashbackHackService cashback= new CashbackHackService();
         int amount=999;
         int actual= cashback.bonus (amount);
@@ -47,7 +47,7 @@ public class CashbackHackServiceTest {
         assertEquals ( expected,actual);
     }
     @Test
-    public void testNotMessangeBonus() {
+    public void ShouldNotBonusNull() {
         CashbackHackService cashback= new CashbackHackService();
         int amount=1000;
         int actual= cashback.bonus (amount);
@@ -55,11 +55,55 @@ public class CashbackHackServiceTest {
         assertEquals ( expected,actual);
     }
     @Test
-    public void testNotMessangeBonus2() {
+    public void ShouldNotBonusMore1000() {
         CashbackHackService cashback= new CashbackHackService();
         int amount=1001;
         int actual= cashback.bonus (amount);
         int expected=0;
         assertEquals ( expected,actual);
     }
+
+    @org.junit.jupiter.api.Test
+    void shouldBonus200T2() {
+        CashbackHackService cashback= new CashbackHackService();
+        int actual =  cashback.bonus(800);
+        assertEquals(200, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldBonus999T2() {
+        CashbackHackService cashback= new CashbackHackService();
+        int actual= cashback.bonus (1);
+        assertEquals ( 999,actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldBonus1000T2() {
+        CashbackHackService cashback= new CashbackHackService();
+        int actual= cashback.bonus (0);
+        assertEquals ( 1000,actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldNegativBonusT2() {
+        CashbackHackService cashback= new CashbackHackService();
+        int actual= cashback.bonus (-1);
+        assertEquals ( 1001,actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldBonus1T2() {
+        CashbackHackService cashback= new CashbackHackService();
+        int actual= cashback.bonus (999);
+        assertEquals ( 1,actual);
+    }
+    @org.junit.jupiter.api.Test
+    void ShouldNotBonusNullT2() {
+        CashbackHackService cashback= new CashbackHackService();
+        int actual= cashback.bonus (1000);
+        assertEquals ( 0,actual);
+    }
+    @org.junit.jupiter.api.Test
+    void ShouldNotBonusMore1000T2() {
+        CashbackHackService cashback= new CashbackHackService();
+        int actual= cashback.bonus (1001);
+        assertEquals ( 0,actual);
+    }
+
 }
